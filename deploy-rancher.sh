981f92f4f68eac18ee05c3d4f01a654f325e3bcb
William Jimenez <william@rancher.com>
2017-06-12 10:56:49 -0700

    use relative path for istioctl

--- deploy-rancher.sh
+++ deploy-rancher.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+set -x
+
+istioctl_bin=$PWD/bin/istioctl
+
 kubectl apply -f install/kubernetes/istio-rbac-alpha.yaml
 kubectl apply -f install/kubernetes/istio.yaml
 kubectl apply -f install/kubernetes/addons/prometheus.yaml
 kubectl apply -f install/kubernetes/addons/grafana.yaml
 kubectl apply -f install/kubernetes/addons/servicegraph.yaml
-kubectl apply -f <(istioctl kube-inject -f samples/apps/bookinfo/bookinfo.yaml)
+kubectl apply -f <($istioctl_bin kube-inject -f samples/apps/bookinfo/bookinfo.yaml)
